e8507a6003d52e3720260a4f22c1d2a9bab9b8d6
Sunkitto <platflow@gmail.com>
2023-09-06 11:39:31 +0200

    Add UiErrorHandler

--- app/src/main/java/com/sunkitto/traveler/feature/UiErrorHandler.kt
+++ app/src/main/java/com/sunkitto/traveler/feature/UiErrorHandler.kt
@@ -2,20 +2,28 @@ package com.sunkitto.traveler.feature
 
 import com.sunkitto.traveler.R
 import com.sunkitto.traveler.common.ResourceManager
+import com.sunkitto.traveler.data.exception.InvalidUserException
+import com.sunkitto.traveler.data.exception.NoEquipmentsFoundException
 import com.sunkitto.traveler.data.exception.NoInternetConnectionException
 import com.sunkitto.traveler.data.exception.ServiceUnavailableException
 import javax.inject.Inject
 
 class UiErrorHandler @Inject constructor(
-    private val resourceManager: ResourceManager
+    private val resourceManager: ResourceManager,
 ) {
 
     fun handleError(exception: Throwable?): String =
-        when(exception) {
+        when (exception) {
             is NoInternetConnectionException ->
                 resourceManager.getString(R.string.no_internet_connection)
             is ServiceUnavailableException ->
                 resourceManager.getString(R.string.service_unavailable)
+            is InvalidUserException -> {
+                resourceManager.getString(R.string.account_was_disabled)
+            }
+            is NoEquipmentsFoundException -> {
+                resourceManager.getString(R.string.no_equipments_found)
+            }
             else ->
                 resourceManager.getString(R.string.unexpected_error)
         }
