a26ba54881fc86f3b0451f55d55fec1de8118cdd
Sunkitto <platflow@gmail.com>
2023-09-06 11:22:25 +0200

    Add DataExceptionHandler

--- app/src/main/java/com/sunkitto/traveler/data/exception/DataExceptionHandler.kt
+++ app/src/main/java/com/sunkitto/traveler/data/exception/DataExceptionHandler.kt
@@ -1,15 +1,20 @@
 package com.sunkitto.traveler.data.exception
 
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import java.io.IOException
 import java.net.UnknownHostException
 import javax.inject.Inject
 
-class DataExceptionHandler @Inject constructor()  {
+/**
+ * Handles exceptions in the data layer.
+ */
+class DataExceptionHandler @Inject constructor() {
 
     fun handleException(exception: Throwable?): Exception =
-        when(exception) {
+        when (exception) {
             is UnknownHostException -> NoInternetConnectionException()
             is IOException -> NoInternetConnectionException()
+            is FirebaseAuthInvalidUserException -> InvalidUserException()
             else -> ServiceUnavailableException()
         }
 }
